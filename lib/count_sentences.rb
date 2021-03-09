ead7b86e8d4ffe24afbb5b251a60bf4f97c924c8
Tim Quimbo <tquimbo1@gmail.com>
2021-03-09 00:00:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".")
   end
 
   def question?
-
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
+    #split sentence with mutiple criterias
+    #split sentence with repeated criteria
+    self.split(/[.|?|!]/).delete_if{|sentence| sentence == ""}.count
 
   end
+
 end
